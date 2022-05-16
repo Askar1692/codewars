7f21e18413ff08b914317fca775f8ecfbcbd07b3
Askar1692 <askar.kusemisov@gmail.com>
2022-05-17 00:03:05 +0600

    added task_12

---
+++ task_12.js
@@ -0,0 +1,27 @@
+/*
+Создайте несколько функций, которые будут возвращать сумму, разность, модуль,
+произведение, частное и показатель степени соответственно.
+ */
+function add(a,b){
+    return a + b
+}
+
+function divide(a,b){
+    return a / b
+}
+
+function multiply(a,b){
+    return a * b
+}
+
+function mod(a,b){
+    return a % b
+}
+
+function exponent(a,b){
+    return a ** b
+}
+
+function subt(a,b){
+    return a - b
+}
